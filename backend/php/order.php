6fdc9049df62a3aa4931f622e50006bc833b53cc
Eliza Lesiewicz <eliza20122002@wp.pl>
2024-04-05 11:23:00 +0200

    dodanie folderu backend

---
+++ backend/php/order.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE HTML>
+<html lang ="pl">
+	<head>
+		<meta charset= "utf-8"/>
+		<title> Cywilizacja na wysokim poziomie zaawansowania technologicznego</title>
+		<meta name="description" content=" Swiadomosc wygenerowana w komputerze na poziomie ludzkiej. Taka, ktora potrafi czuc, myslec, mowic w taki sam sposob jak my."/>
+		<meta name="keywords" content="filozofia,nowoczesna cywilizacja,samodzielna cywilizacja komputerow,ludzie,people,philosophia,mysli,uczucia"/>
+		<meta name="author" content="Gruparobocza3"/>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+	</head>
+	<body bgcolor="#303030" text="white">
+
+	<?php
+		echo "Formularz zostal wyslany. Dziekujemy za Twoja opinie.";
+
+	?>
+
+		<header>
+			<nav>
+
+			</nav>
+
+		</header>
+
+		<main>
+
+			<article>
+
+			</article>
+
+			<section>
+
+			</section>
+
+		</main>
+
+
+
+	</body>
+
+</html>
